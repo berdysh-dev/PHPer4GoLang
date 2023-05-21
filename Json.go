084581aee286e0570104ac1a9b4c742d7b3ec66d
root <root@berdysh.net>
2023-05-21 14:02:44 +0900

    Test

---
+++ Json.go
@@ -0,0 +1,29 @@
+package PHPer4GoLang
+
+import (
+    "errors"
+)
+
+func Json_decode(i ... interface{}) (interface{},error){
+
+    var ret interface{} ;
+
+    err := errors.New("ASSERT") ;
+
+    return ret , err ;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
