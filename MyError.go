bf2efcd7030f65eebe196d57ad7bcc7ec370a413
root <root@berdysh.net>
2023-09-09 01:19:06 +0900

    Test

--- MyError.go
+++ MyError.go
@@ -21,18 +21,22 @@ func (err *MyErrorStruct) Error() (string) {
     }
 }
 
+func (err *MyErrorStruct) Line() (string) {
+    return "HOHO" ;
+}
+
 func Err(v ... any) (error) {
     var err MyErrorStruct ;
 
     argc := len(v) ;
-    off := 0 ;
+    n := 0 ;
     flag := 1 ;
 
     if(argc > 0){
         if(reflect.ValueOf(v[0]).Kind() == reflect.Int){
             flag = v[0].(int) ;
             argc -= 1 ;
-            off += 1 ;
+            n += 1 ;
         }
     }
 
@@ -40,9 +44,13 @@ func Err(v ... any) (error) {
 
     switch(argc){
     case 0: return nil ;
-    case 1: err.mess = v[off].(string) ;
-    case 2: err.mess = fmt.Sprintf(v[off].(string),v[off+1]) ;
-    case 3: err.mess = fmt.Sprintf(v[off].(string),v[off+1],v[off+2]) ;
+    case 1: err.mess = v[n].(string) ;
+    case 2: err.mess = fmt.Sprintf(v[n].(string),v[n+1]) ;
+    case 3: err.mess = fmt.Sprintf(v[n].(string),v[n+1],v[n+2]) ;
+    case 4: err.mess = fmt.Sprintf(v[n].(string),v[n+1],v[n+2],v[n+3]) ;
+    case 5: err.mess = fmt.Sprintf(v[n].(string),v[n+1],v[n+2],v[n+3],v[n+4]) ;
+    case 6: err.mess = fmt.Sprintf(v[n].(string),v[n+1],v[n+2],v[n+3],v[n+4],v[n+5]) ;
+    case 7: err.mess = fmt.Sprintf(v[n].(string),v[n+1],v[n+2],v[n+3],v[n+4],v[n+5],v[n+6]) ;
     }
 
     pc, file, line, ok := runtime.Caller(1) ;
